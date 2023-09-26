230e2eec4e6cd35286f94ae33f137e1a6145109c
SeongHyunJeong <sean3080211@gmail.com>
2023-09-26 14:21:38 +0900

    박찬홍 뒤질래

--- Assets/Scripts/G29CarController.cs
+++ Assets/Scripts/G29CarController.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-public class CarController : MonoBehaviour
+public class G29CarController : MonoBehaviour
 {
     private WheelCollider[] wheelColliders;
     private GameObject[] wheels;
     private Rigidbody rb;
-    
-    public float power;
-    public float breakPower;
+
+    [Header("Padel")]
+    public float power; //속력
+    private float accPower; //엑셀범위
+    private int intAccP;
+    private int isAcc;
+
+    public float breakPower; //브레이크 범위값
     private float wheelBase; //앞 뒤 바퀴 사이의 거리(m단위)
     private float rearTrack; //좌 우 바튀 사이의 거리(m단위)
-    public float turnRadius; //회저 반지럼(m단위)
+    public float turnRadius; //회전 반지름(m단위)
+
+    [Header("Handle")]
+    private float ro; //바퀴 회전값
+    private float isro;
 
     public bool isBreak;
     // Start is called before the first frame update
@@ -27,14 +34,14 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SteerVehicle(); //애커만 조향
+        WheelPosWithCollider();  //바퀴의 위치와 로테이션 값을 항상 같게 만들어줌
+        InputLogitech();
         //브레이크 기능(자동차 브레이크시 4개의 바퀴에 모두 적용되어야함)
         for (int i = 0; i < wheels.Length; i++)
         {
-            // for문을 통해서 휠콜라이더 전체를 Vertical 입력에 따라서 power만큼의 힘으로 움직이게한다.
-            wheelColliders[i].motorTorque = Input.GetAxis("Vertical") * power;
-
-           
-
+            // for문을 통해서 휠콜라이더 전체를 엑셀 입력에 따라서 power만큼의 힘으로 움직이게 한다.
+            wheelColliders[i].motorTorque = isAcc * power;
         }
 
         if (breakPower > 32767)
@@ -51,19 +58,37 @@ public class CarController : MonoBehaviour
             wheel.brakeTorque = 3 * breakPower;
         }
 
-        SteerVehicle(); //애커만 조향
-        WheelPosWithCollider();  //바퀴의 위치와 로테이션 값을 항상 같게 만들어줌
-
     }
-    void Update()
+
+    void InputLogitech()
     {
-        //LogitechGSDK.DIJOYSTATE2ENGINES rec;
-        //rec = LogitechGSDK.LogiGetStateUnity(0);
-        //ro = rec.lX / 819;
-        //transform.rotation = Quaternion.Euler(0, ro, 0);
-        //Debug.Log(speed);
-    }
+        LogitechGSDK.DIJOYSTATE2ENGINES rec;
+        rec = LogitechGSDK.LogiGetStateUnity(0);
+        ro = rec.lX / 10000;
+        accPower = Mathf.Abs(rec.lY - 32767);
+        intAccP = (int)accPower / 10000;
+        if (rec.lY != 0)
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                if (rec.rgbButtons[i] == 128)
+                {
+                    if (i == 14) isAcc = 1;
+                    else if (i == 15) isAcc = -1;
+                }
 
+            }
+        }
+        if (rec.lRz != 0)
+        {
+            breakPower = Mathf.Abs(rec.lRz - 32767);
+        }
+        if (ro > 0) isro = 1;
+        else if (ro < 0) isro = -1;
+        else if (ro == 0) isro = 0;
+        //Button status :
+
+    }
 
     //바퀴 mesh와 collider의 위치를 맞춰줌
     void WheelPosWithCollider()
@@ -84,29 +109,55 @@ public class CarController : MonoBehaviour
     void SteerVehicle()
     {
         //steerAngle = 바퀴의 조향 각도
-        
+
         //공식 Rad2Deg * Atan(wheelBase in meter / (turnRadius in meters + (rearTrack in meters / 2to get center)) * steerInput  left
         // Rad2Deg* Atan(wheelBase in meter / (turnRadius in meters - (rearTrack in meters / 2to get center)) *steerInput  right
-
+        //transform.rotation = Quaternion.Euler(0, ro, 0); 
         //바퀴 오른쪽으로 회전
-        if (Input.GetAxis("Horizontal") > 0)
+        if (isro > 0)
         {
+            //Debug.Log("우회전");
             //Rad2Deg -> 각도를 라디안에서 degree(도)로 변환
-            wheelColliders[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * Input.GetAxis("Horizontal");
-            wheelColliders[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * Input.GetAxis("Horizontal");
-            //Debug.Log(wheelColliders[0].steerAngle);
+            wheelColliders[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * isro;
+            wheelColliders[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * isro;
         }
         //바퀴 왼쪽으로 회전
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (isro < 0)
         {
-            wheelColliders[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * Input.GetAxis("Horizontal");
-            wheelColliders[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * Input.GetAxis("Horizontal");
+            //Debug.Log("좌회전");
+            wheelColliders[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * isro;
+            wheelColliders[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * isro;
             //Debug.Log(wheelColliders[0].steerAngle);
         }
         else
         {
+            //Debug.Log("직진");
             wheelColliders[0].steerAngle = 0;
             wheelColliders[1].steerAngle = 0;
+        }
+        switch (intAccP)
+        {
+            case 0:
+                power = 0;
+                break;
+            case 1:
+                power = 100;
+                break;
+            case 2:
+                power = 150;
+                break;
+            case 3:
+                power = 200;
+                break;
+            case 4:
+                power = 250;
+                break;
+            case 5:
+                power = 300;
+                break;
+            case 6:
+                power = 350;
+                break;
         }
     }
 
@@ -117,8 +168,6 @@ public class CarController : MonoBehaviour
         wheels = new GameObject[4];
         rb = GetComponentInParent<Rigidbody>();
         rb.centerOfMass = new Vector3(0, -1, 0); //무게중심을 y축 방향으로 낮춤
-        power = 200f;
-
         wheels[0] = GameObject.FindGameObjectWithTag("FRWheel");
         wheels[1] = GameObject.FindGameObjectWithTag("FLWheel");
         wheels[2] = GameObject.FindGameObjectWithTag("RRWheel");
@@ -137,6 +186,4 @@ public class CarController : MonoBehaviour
         wheelBase = wheelColliders[1].transform.position.z - wheelColliders[3].transform.position.z;
         rearTrack = wheelColliders[2].transform.position.x - wheelColliders[3].transform.position.x;
     }
-
-  
 }
